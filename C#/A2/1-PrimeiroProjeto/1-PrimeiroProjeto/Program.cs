f1f07a6be2d915069a1508851a6bfc286853d9fc
Wenklesley Mendes <wenklesley.mendes@gmail.com>
2020-11-21 18:51:48 -0300

    Iniciando Aula C#

---
+++ C#/A2/1-PrimeiroProjeto/1-PrimeiroProjeto/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, Mundo. Projeto no Visual Studio ");
+
+            Console.WriteLine("Aexecução acabou. Tecle enter para finalizar...");
+            Console.ReadLine();
+        }
+    }
+}
