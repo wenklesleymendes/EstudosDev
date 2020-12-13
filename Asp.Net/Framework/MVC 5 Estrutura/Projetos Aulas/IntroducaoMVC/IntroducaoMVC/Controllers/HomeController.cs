c838d93683e2f472bd54b1194fb5beae4e1c3c91
Wenklesley Mendes <wenklesley.mendes@gmail.com>
2020-12-13 16:42:26 -0300

    Atualizado 13-12-2020 parte da tarde

--- Asp.Net/Framework/MVC 5 Estrutura/Projetos Aulas/IntroducaoMVC/IntroducaoMVC/Controllers/HomeController.cs
+++ Asp.Net/Framework/MVC 5 Estrutura/Projetos Aulas/IntroducaoMVC/IntroducaoMVC/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
 ﻿using IntroducaoMVC.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace IntroducaoMVC.Controllers
@@ -38,6 +34,50 @@ namespace IntroducaoMVC.Controllers
             ViewBag.Tipo = pessoaViewBag.Tipo;
 
             return View();
+        }
+
+        public ActionResult IndexVDB()
+        {
+            var pessoaViewData = new Pessoa()
+            {
+                RepresentacaoDados = "ViewData",
+                PesssoaId = 1,
+                Nome = "Wenklesley Mendes Oliveira",
+                Tipo = "Desenvolvedor"
+            };
+
+            ViewData["RepresentacaoDados"] = pessoaViewData.RepresentacaoDados;
+            ViewData["PesssoaId"] = pessoaViewData.PesssoaId;
+            ViewData["Nome"] = pessoaViewData.Nome;
+            ViewData["Tipo"] = pessoaViewData.Tipo;
+
+            var pessoaViewBag = new Pessoa()
+            {
+                RepresentacaoDados = "ViewBag",
+                PesssoaId = 2,
+                Nome = "Kaio Nathan Silva Mendes",
+                Tipo = "Professor de Linguas estrangeiras"
+            };
+
+            ViewBag.Dados = pessoaViewBag.RepresentacaoDados;
+            ViewBag.Id = pessoaViewBag.PesssoaId;
+            ViewBag.Nome = pessoaViewBag.Nome;
+            ViewBag.Tipo = pessoaViewBag.Tipo;
+
+            return View();
+        }
+
+        public ActionResult IndexVTP()
+        {
+            var pessoaViewTipada = new Pessoa()
+            {
+                RepresentacaoDados = "ViewTipada",
+                PesssoaId = 3,
+                Nome = "Henrry Gabriel Silva Mendes",
+                Tipo = "Administrador Financeiro"
+            };
+
+            return View(pessoaViewTipada);
         }
 
         public  ActionResult Contatos()
