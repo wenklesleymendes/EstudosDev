3ecfee838cb06a3f10d14fa42f9e27eae69a6213
Wenklesley Mendes <wenklesley.mendes@gmail.com>
2020-12-13 10:52:13 -0300

    Atualização 13-12-2020

--- Asp.Net/Framework/MVC 5 Estrutura/Projetos Aulas/IntroducaoMVC/IntroducaoMVC/Controllers/HomeController.cs
+++ Asp.Net/Framework/MVC 5 Estrutura/Projetos Aulas/IntroducaoMVC/IntroducaoMVC/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IntroducaoMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,32 @@ namespace IntroducaoMVC.Controllers
     {
         public ActionResult Index()
         {
+            var pessoaViewData = new Pessoa()
+            {
+                RepresentacaoDados = "ViewData",
+                PesssoaId = 1,
+                Nome = "Wenklesley Mendes Oliveira",
+                Tipo = "Desenvolvedor"
+            };
+
+            ViewData["RepresentacaoDados"] = pessoaViewData.RepresentacaoDados;
+            ViewData["PesssoaId"] = pessoaViewData.PesssoaId;
+            ViewData["Nome"] = pessoaViewData.Nome;
+            ViewData["Tipo"] = pessoaViewData.Tipo;
+
+            var pessoaViewBag = new Pessoa()
+            {
+                RepresentacaoDados = "ViewBag",
+                PesssoaId = 2,
+                Nome = "Kaio Nathan Silva Mendes",
+                Tipo = "Professor de Linguas estrangeiras"
+            };
+
+            ViewBag.Dados = pessoaViewBag.RepresentacaoDados;
+            ViewBag.Id= pessoaViewBag.PesssoaId;
+            ViewBag.Nome = pessoaViewBag.Nome;
+            ViewBag.Tipo = pessoaViewBag.Tipo;
+
             return View();
         }
 
---
+++ Asp.Net/Framework/MVC 5 Estrutura/Projetos Aulas/IntroducaoMVC/IntroducaoMVC/Models/Pessoa.cs
@@ -0,0 +1,10 @@
+﻿namespace IntroducaoMVC.Models
+{
+    public class Pessoa
+    {
+        public string RepresentacaoDados { get; set; }
+        public int PesssoaId { get; set; }
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+    }
+}
