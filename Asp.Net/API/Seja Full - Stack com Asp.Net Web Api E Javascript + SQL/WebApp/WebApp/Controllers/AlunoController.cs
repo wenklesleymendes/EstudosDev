9093122376f8418e86838d70104f80927a6a6446
Wenklesley Mendes <wenklesley.mendes@gmail.com>
2020-12-28 17:07:31 -0300

    Atualização 28/12/2020  as 17:07

---
+++ Asp.Net/API/Seja Full - Stack com Asp.Net Web Api E Javascript + SQL/WebApp/WebApp/Controllers/AlunoController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class AlunoController : ApiController
+    {
+        // GET: api/Aluno
+        public IEnumerable<Aluno> Get()
+        {
+            return new Aluno().ListaAlunos();
+        }
+
+        // GET: api/Aluno/5
+        public Aluno Get(int id)
+        {
+            var alunos = new Aluno();
+            return alunos.ListaAlunos().Where(a => a.Id == id).FirstOrDefault();
+        }
+
+        // POST: api/Aluno
+        public List<Aluno> Post(Aluno aluno)
+        {
+            var _aluno = new Aluno();
+            _aluno.Inserir(aluno);
+
+            return _aluno.ListaAlunos();
+        }
+
+        // PUT: api/Aluno/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Aluno/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Asp.Net/API/Seja Full - Stack com Asp.Net Web Api E Javascript + SQL/WebApp/WebApp/Models/Aluno.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace WebApp.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public string Telefone { get; set; }
+        public string Ra { get; set; }
+
+        public List<Aluno> ListaAlunos()
+        {
+            var caminhoArquivo = HostingEnvironment.MapPath(@"~/App_Data\Base.json");
+            var json = File.ReadAllText(caminhoArquivo);
+            return JsonConvert.DeserializeObject<List<Aluno>>(json); ;
+        }
+
+        public bool ReescrevarArquivo(List<Aluno> listaAlunos)
+        {
+            var caminhoArquivo = HostingEnvironment.MapPath(@"~/App_Data\Base.json");
+            var json = JsonConvert.SerializeObject(listaAlunos, Formatting.Indented);
+            File.WriteAllText(caminhoArquivo, json);
+            return true;
+        }
+
+        public Aluno Inserir(Aluno aluno)
+        {
+            var listaAlunos = this.ListaAlunos();
+            var maxId = listaAlunos.Max(p => p.Id);
+            aluno.Id = maxId + 1;
+            listaAlunos.Add(aluno);
+
+            ReescrevarArquivo(listaAlunos);
+
+            return aluno;
+        }
+
+        public Aluno Atualizar(int idAluno, Aluno aluno)
+        {
+            var listaAlunos = this.ListaAlunos();
+            var indiceAluno = listaAlunos.FindIndex(p => p.Id == aluno.Id);
+            if (indiceAluno >= 0)
+            {
+                aluno.Id = idAluno;
+                listaAlunos[indiceAluno] = aluno;
+            }
+            else
+            {
+                return null;
+            }
+
+            ReescrevarArquivo(listaAlunos);
+            return aluno;
+        }
+
+        public bool Deletar(int idAluno)
+        {
+            var listaAlunos = this.ListaAlunos();
+            var indiceAluno = listaAlunos.FindIndex(p => p.Id == idAluno);
+            if (indiceAluno >= 0)
+            {
+                listaAlunos.RemoveAt(indiceAluno);
+            }
+            else
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
