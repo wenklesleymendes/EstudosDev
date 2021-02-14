d1a2a737e966b77d14684e2311f4d563599f05d3
Wenklesley Mendes <wenklesley.mendes@gmail.com>
2021-02-13 23:03:20 -0300

    Atualizaçao 13-02-2021 v1

--- Asp.Net/API/Seja Full - Stack com Asp.Net Web Api E Javascript + SQL/WebApp/WebApp/Controllers/AlunoController.cs
+++ Asp.Net/API/Seja Full - Stack com Asp.Net Web Api E Javascript + SQL/WebApp/WebApp/Controllers/AlunoController.cs
@@ -7,10 +7,13 @@ using WebApp.Models;
 
 namespace WebApp.Controllers
 {
-    [EnableCors("*","*","*")]
+    [EnableCors("*", "*", "*")]
+    [RoutePrefix("api/Aluno")]
     public class AlunoController : ApiController
     {
         // GET: api/Aluno
+        [HttpGet]
+        [Route("Recuperar")]
         public IHttpActionResult Get()
         {
             try
@@ -22,14 +25,42 @@ namespace WebApp.Controllers
             {
                 return InternalServerError(ex);
             }
-            
+
         }
 
         // GET: api/Aluno/5
+        [HttpGet]
+        [Route("Recuperar")]
         public Aluno Get(int id)
         {
             var alunos = new Aluno();
             return alunos.ListaAlunos().Where(a => a.Id == id).FirstOrDefault();
+        }
+
+        // GET: api/Aluno/5
+        [HttpGet]
+        [Route(@"RecuperarPorDataNome/{data:regex([0-9]{4}\-[0-9])}/{nome:minlength(5)}")]
+        public IHttpActionResult Recuperar(string data, string nome)
+        {
+            try
+            {
+                var aluno = new Aluno();
+                IEnumerable< Aluno> alunos = aluno.ListaAlunos().Where(a => a.Data == data || a.Nome == nome);
+
+                if (!alunos.Any())
+                {
+                    return NotFound();
+                }
+
+                return Ok(alunos);
+
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+                throw;
+            }
+
         }
 
         // POST: api/Aluno
@@ -42,7 +73,7 @@ namespace WebApp.Controllers
         }
 
         // PUT: api/Aluno/5
-        public Aluno Put(int id, [FromBody]Aluno aluno)
+        public Aluno Put(int id, [FromBody] Aluno aluno)
         {
             var _aluno = new Aluno();
             return _aluno.Atualizar(id, aluno);
--- Asp.Net/API/Seja Full - Stack com Asp.Net Web Api E Javascript + SQL/WebApp/WebApp/Models/Aluno.cs
+++ Asp.Net/API/Seja Full - Stack com Asp.Net Web Api E Javascript + SQL/WebApp/WebApp/Models/Aluno.cs
@@ -14,6 +14,7 @@ namespace WebApp.Models
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
         public string Telefone { get; set; }
+        public string Data { get; set; }
         public string RA { get; set; }
 
         public List<Aluno> ListaAlunos()
